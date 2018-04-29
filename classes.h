fd74ded13844cf54a9e48032ae549ab098419102
Preet-Jyotpreet <38120430+Preet-Jyotpreet@users.noreply.github.com>
2018-04-28 20:59:45 -0700

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class One{
+         public static void main(String[] args) {
+             Scanner in = new Scanner(System.in);
+            // String answer_one;
+        System.out.println("What is right syntax for cin? ");
+          String name = in.nextLine();
+            System.out.println("You Entered: " +name);
+
+
+
+    }
+}
 
