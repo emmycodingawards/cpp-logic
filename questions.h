f245db8b9a45fb28bb9a07ec9d3bf470f712a25c
Preet-Jyotpreet <38120430+Preet-Jyotpreet@users.noreply.github.com>
2018-05-01 19:51:40 -0700

    Update questions.h

--- questions.h
+++ questions.h
@@ -17,7 +17,7 @@ public class One{
             else {
                 System.out.println("Oops! YOU ARE SCREWED.");   }
 
-            System.out.flush();
+            System.out.print("\f");
 
         System.out.println("2.What is right syntax for cout? ");
           String name2 = in.nextLine();
@@ -33,7 +33,35 @@ public class One{
             System.out.println("Hey, it looks like you're trying to do a cout statement. In order to do a cout statement, you want to type something like this: cout << x; Note that it looks like there are arrows pointing FROM the variable TO cout. I think you might have done yours backwards (cout >> x; is wrong).."); }
              else {
                 System.out.println("Oops! YOU ARE SCREWED.");   }
-
+                  
+                  System.out.print("\f");
+       
+                  
+              System.out.println("3. Why would the code below not compile?");
+              System.out.println("int x;");
+              System.out.println("cin >> x;");
+              System.out.println("cout << x << endl");
+               System.out.println(" "); 
+               System.out.println("Choose an Appropriate answer:");   
+                 System.out.println("A. Wrong cin Arrows.");
+                 System.out.println("B. uninitialized variable.");
+                 System.out.println("C. Missing a Semicolon.");
+                 String name3 = in.nextLine();
+             if (name3.equals( "C") ) {
+                System.out.println("Your answer is correct."); }
+            else if (name3.equals( "c") ) {
+                System.out.println("Your answer is correct."); }
+            else if (name3.equals( "A") ) {
+                System.out.println("I know it is disappointing, but ARROWS are perfectly fine."); }
+            else if (name3.equals( "a") ) {
+                System.out.println("I know it is disappointing, but ARROWS are perfectly fine."); }
+            esle if (name3.equals( "B") ) {
+                System.out.println("Hmm ... that can be an issue, but NOT this time."); }
+            else if (name3.equals( "b") ) {
+                System.out.println("Hmm ... that can be an issue, but NOT this time."); }
+            else {
+                  System.out.println("Oops! YOU ARE SCREWED.");   }
+                  
     }
 }
 
