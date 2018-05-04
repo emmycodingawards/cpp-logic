23f8c968f6e47d74b52d5d744f3556416b20e617
Chris Vienna <tanker242@gmail.com>
2018-05-04 04:04:55 -0700

    pushed bitfield

--- Bitfield.java
+++ Bitfield.java
@@ -1,1 +1,33 @@
+// WIP Chris V.
+public class Bitfield {
+
+
+	public static enum Flag {
+	
+	}
+
+
+
+
+}
+
+public boolean isEnabled(final int flag) {
+	        return (current & flag) == flag;
+			    }
+
+    public int current() {
+		        return current;
+				    }
+
+    public void enable(final int flag) {
+		        current |= flag;
+				    }
+
+    public void delete(final int flag) {
+		        current &= ~flag;
+				    }
+
+    public void toggle(final int flag) {
+		        current ^= flag;
+				    }
 
