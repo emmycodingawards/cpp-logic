6a1a8c5502ea1c6b9ab0ea0344683252d0d1c77d
chris mendonca <38120395+chrismendonca@users.noreply.github.com>
2018-05-07 09:30:41 -0700

    Update LL.java

--- LL.java
+++ LL.java
@@ -1,112 +1,113 @@
-//WIP - Chris M
-
 class LL{
 
-        private ListNode head;
-        private ListNode tail;
-        private int length_of_LL;
+	private ListNode head;
+	private ListNode tail;
+	private int length_of_LL;
 
-        private class ListNode{
-                private int data;
-                private ListNode next;
-                private ListNode previous;
+	private class ListNode{
+		private int data;
+		private String str;
+		private ListNode next;
+		private ListNode previous;
 
-                public ListNode(int data){
-                        this.data = data;
-                }
-        }
+		public ListNode(int data, String str){
+			this.data = data;
+			this.str = str;
+		}
+	}
 
-        public LL(){
-                this.head = null;
-                this.tail = null;
-                this.length_of_LL = 0;
-        }
+	public LL(){
+		this.head = null;
+		this.tail = null;
+		this.length_of_LL = 0;
+	}
 
-        public boolean isEmpty(){ //returns true if list is empty
-                return length_of_LL == 0;
-        }
+	public boolean isEmpty(){return length_of_LL == 0;}
 
-        public int length_of_LL(){
-                return length_of_LL;
-        }
+	//public void die(){return 0;}
 
-        public void display_forward(){
-                if(head == null){return;}
-                ListNode temp = head;
-                while(temp != null){
-                        System.out.print(temp.data + " -> ");
-                        temp = temp.next;
-                }
-                System.out.println("null");
-        }
+	public int length_of_LL(){
+		return length_of_LL;
+	}
 
-        public void display_backward(){
-                if(tail == null){return;}
-                ListNode temp == tail;
-                while(temp != null){
-                        System.out.print(temp.data + " -> ");
-                        temp = temp.previous;
-                }
-                System.out.println("null");
-        }
-                
-        public void insert_first(int value){ //insert to the front of the LL
-                ListNode newNode = new ListNode(value);
-                if(isEmpty()){tail = newNode;}
-                else{head.previous = newNode;}
-                newNode.next = head;
-                head = newNode;
-                length_of_LL++;
-        }
+	public void display_forward(){
+		if(head == null){return;}
+		ListNode temp = head;
+		while(temp != null){
+			System.out.print(temp.data + ", " + temp.str + " -> ");
+			temp = temp.next;
+		}
+		System.out.println("null");
+	}
 
-        public insert_last(int value){ //insert at the end of the LL
-                ListNode newNode = new ListNode(value);
-                if(isEmpty()){head = newNode;}
-                else{
-                        tail.next = newNode;
-                        newNode.previous = tail;
-                }
-                tail = newNode;
-                length++;
-        }
+	public void display_backward(){
+		if(tail == null){return;}
+		ListNode temp = tail;
+		while(temp != null){
+			System.out.print(temp.data + ", " + temp.str + " -> ");
+			temp = temp.previous;
+		}
+		System.out.println("null");
+	}
 
-        public ListNode delete_first(){
-                if(isEmpty()){throw new does_not_exist();} //need to look up java exception handling
-                ListNode temp = head;
-                if(head == tail){tail = null;}
-                else{head.next.previous = null;}
-                head = head.next;
-                temp.next = null;
-                length--;
-                return temp;
-        }
+	public void insert_first(int value, String str){ //insert to the front of the LL
+		ListNode newNode = new ListNode(value, str);
+		if(isEmpty()){tail = newNode;}
+		else{head.previous = newNode;}
+		newNode.next = head;
+		head = newNode;
+		length_of_LL++;
+	}
 
-        public ListNode delete_last(){
-                if(isEmpty()){throw new does_not_exist();} // and again
-                ListNode temp = tail;
-                if(head == tail){head = null;}
-                else{tail.previous.next = null;}
-                tail = tail.previous;
-                temp.previous = null;
-                length--;
-                return temp;
-        }
+	public void insert_last(int value, String str){ //insert at the end of the LL
+		ListNode newNode = new ListNode(value, str);
+		if(isEmpty()){head = newNode;}
+		else{
+			tail.next = newNode;
+			newNode.previous = tail;
+		}
+		tail = newNode;
+		length_of_LL++;
+	}
 
-        public static void main(String[] args){
-                LL cpp_ll = new LL();
-                
-                //change these values to populate the list
-                cpp_ll.insert_first(1);
-                cpp_ll.insert_first(2);
-                cpp_ll.insert_first(3);
-                
-                
-                //test post, do not upvote
-                //cpp_ll.display_forward();
-                //cpp_ll.display_backward();
-                //cpp_ll.delete_first();
-                //cpp_ll.display_forward();
-                //cpp_ll.delete_last();
-                //cpp_ll.display_forward();
-                
-        }
+	public ListNode delete_first(){
+		//if(isEmpty()){die();} //need to look up java exception handling
+		ListNode temp = head;
+		if(head == tail){tail = null;}
+		else{head.next.previous = null;}
+		head = head.next;
+		temp.next = null;
+		length_of_LL--;
+		return temp;
+	}
+
+	public ListNode delete_last(){
+		//if(isEmpty()){die();} // and again
+		ListNode temp = tail;
+		if(head == tail){head = null;}
+		else{tail.previous.next = null;}
+		tail = tail.previous;
+		temp.previous = null;
+		length_of_LL--;
+		return temp;
+	}
+
+	public static void main(String[] args){
+		LL cpp_ll = new LL();
+
+		//change these values to populate the list
+		cpp_ll.insert_first(3, "question three");
+		cpp_ll.insert_first(2, "question two");
+		cpp_ll.insert_first(1, "question one");
+
+
+		//test post, do not upvote
+		cpp_ll.display_forward();
+		cpp_ll.display_backward();
+		cpp_ll.delete_first();
+		cpp_ll.display_forward();
+		cpp_ll.delete_last();
+		cpp_ll.display_forward();
+
+	}
+}
